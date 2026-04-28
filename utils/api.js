6d1eb0a01e3d067f89262c2d1460a4499de04693
grantmontain <157995650+grantmontain@users.noreply.github.com>
2026-04-28 12:52:44 -0300

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -89,7 +89,7 @@ const APIs = {
   // Random Meme
   getMeme: async () => {
     try {
-      const response = await api.get('https://meme-api.com/gimme/brasil');
+      const response = await api.get('https://meme-api.com/gimme/NaoMortoAbraDentro');
       return response.data;
     } catch (error) {
       throw new Error('Failed to fetch meme');
