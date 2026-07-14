5ae87099d3dde82d6cc0a9dcd7b62736c9711a04
grantmontain <bonfim.carlos.200@gmail.com>
2026-07-14 16:07:28 -0300

    fsadafs

--- config.js
+++ config.js
@@ -4,11 +4,11 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['9876543210'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['5586981006669'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['Montanha'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
-    botName: 'Knight Bot Mini',
+    botName: 'K7',
     prefix: ',',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
@@ -16,7 +16,7 @@ module.exports = {
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
-    packname: 'Knight Bot',
+    packname: 'K7 Bot',
     
     // Bot Behavior
     selfMode: false, // Private mode - only owner can use commands
