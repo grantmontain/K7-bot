4fbd55beb8803a077319821aeb6aabd32563022f
grantmontain <157995650+grantmontain@users.noreply.github.com>
2026-07-28 20:48:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     
     // Bot Configuration
     botName: 'K7',
-    prefix: ',',
+    prefix: '.',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
     newsletterJid: '120363161513685998@newsletter', // Newsletter JID for menu forwarding
